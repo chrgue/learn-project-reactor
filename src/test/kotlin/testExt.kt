353bc8260273deb9fd42c707578e3c21dc33ca32
Christian Günther <17281422+chrgue@users.noreply.github.com>
2021-09-21 09:04:42 +0200

    showcase flatMapSequential

--- src/test/kotlin/testExt.kt
+++ src/test/kotlin/testExt.kt
@@ -1,5 +1,11 @@
 import org.reactivestreams.Publisher
+import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
+import java.time.Duration
+import kotlin.random.Random
 
 fun <T> Publisher<T>.stepVerify() =
     StepVerifier.create(this)
+
+fun <T> Mono<T>.delayRandom() =
+    delayElement(Duration.ofMillis(Random.nextLong(0, 10)))
